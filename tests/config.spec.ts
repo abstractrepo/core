7129d1ebb28d6f399d6816babd9f5d11acc1fdc9
KimHout Sam <samkimhout@gmail.com>
2024-05-21 07:32:03 +0700

    test: test `defineRootConfig`

--- tests/config.spec.ts
+++ tests/config.spec.ts
@@ -1,10 +1,31 @@
-import { GetDefineRootConfigLayout } from "../src/config/root";
+import defineRootConfig from "../src/config/root";
 import { default as _config } from "./mockRootConfig";
-
-type ConfigLayout = GetDefineRootConfigLayout<typeof _config>;
 
 describe("config should", () => {
   it("defineRootConfig returns Root Config", async () => {
     const config = await _config;
+
+    expect(
+      await defineRootConfig({
+        projects: [
+          {
+            name: "playground-001",
+            path: "<base_path>/playgrounds",
+            type: "playground",
+          },
+        ],
+
+        commands: [],
+
+        expansion: [],
+
+        templates: [],
+
+        layouts: {
+          project: "<base_path>/project",
+          playground: "<base_path>/playground",
+        },
+      })
+    ).toStrictEqual(config);
   });
 });
