7be2fdb1e6f12ee9bcae125d66d2dff8a312657a
KimHout Sam <samkimhout@gmail.com>
2024-06-02 05:12:41 +0700

    feat: add method to TemplateHelper

--- src/config/template/helper.ts
+++ src/config/template/helper.ts
@@ -1,1 +1,31 @@
-export default class TemplateHelper {}
+import { withFallback } from "../../utils";
+import { TemplateConfig } from "./types";
+
+export default class TemplateHelper {
+  #raw: TemplateConfig;
+
+  constructor(templateConfig: TemplateConfig) {
+    const config = {
+      ...templateConfig,
+      templateCanBeTag: withFallback(templateConfig.templateCanBeTag, []),
+    } as TemplateConfig;
+
+    this.#raw = config;
+
+    return this;
+  }
+
+  /**
+   * get template that group in project tags
+   */
+  get getTemplateCanBeTag() {
+    return this.#raw.templateCanBeTag;
+  }
+
+  /**
+   * get template that group in project types
+   */
+  get getTemplateCanBeType() {
+    return this.#raw.templateCanBeType;
+  }
+}
