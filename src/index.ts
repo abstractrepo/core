f71dac90ee831383d36fa3ad3acd5f2ebc4913ab
KimHout Sam <samkimhout@gmail.com>
2024-06-02 05:26:38 +0700

    chore: export some function from utils.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { type RootConfig, defineRootConfig, RootHelper } from "./config/root";
 export { type TemplateConfig, defineTemplateConfig } from "./config/template";
+export { toArray, withFallback } from "./utils";
