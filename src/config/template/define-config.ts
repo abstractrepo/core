6ca5c5e1e84ff89840d14b9d7f14230ba3351ec7
KimHout Sam <samkimhout@gmail.com>
2024-06-02 05:14:08 +0700

    feat: using helper and throw if required property not specified

--- src/config/template/define-config.ts
+++ src/config/template/define-config.ts
@@ -1,5 +1,12 @@
+import TemplateHelper from "./helper";
 import { TemplateConfig } from "./types";
 
-export default function defineTemplateConfig(TemplateConfig: TemplateConfig) {
-  return TemplateConfig;
+export default function defineTemplateConfig(templateConfig: TemplateConfig) {
+  if (!templateConfig.templateCanBeType) {
+    throw new ReferenceError(
+      "E_CANNOT_FIND_TEMPLATE_CAN_BE_TYPE: cannot find `templateCanBeType` in config"
+    );
+  }
+
+  return new TemplateHelper(templateConfig);
 }
