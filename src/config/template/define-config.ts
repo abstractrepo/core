5c14add41459547385e7d3a8fd213201e8b969ab
KimHout Sam <samkimhout@gmail.com>
2024-06-01 18:34:04 +0700

    refactor: using `rootHelper` rather define types itself

--- src/config/template/define-config.ts
+++ src/config/template/define-config.ts
@@ -1,10 +1,5 @@
-import { GlobalProgrammingType } from "../../global";
 import { TemplateConfig } from "./types";
 
-export default function defineTemplateConfig<
-  TProgramming extends keyof GlobalProgrammingType,
-  TRuntime extends keyof GlobalProgrammingType[TProgramming],
-  TPackageManager extends GlobalProgrammingType[TProgramming][TRuntime]
->(TemplateConfig: TemplateConfig<TProgramming, TRuntime, TPackageManager>) {
+export default function defineTemplateConfig(TemplateConfig: TemplateConfig) {
   return TemplateConfig;
 }
