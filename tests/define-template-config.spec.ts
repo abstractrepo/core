0a9a6988a1c77a6d55c87b5257f198cfdf53a804
KimHout Sam <samkimhout@gmail.com>
2024-06-10 09:14:47 +0700

    test: just add todo

--- tests/define-template-config.spec.ts
+++ tests/define-template-config.spec.ts
@@ -2,50 +2,50 @@ import { defineRootConfig } from "../src";
 import { TemplateConfig, defineTemplateConfig } from "../src/config/template";
 
 describe("test: `defineTemplateConfig` should pass", () => {
-    const basicRootConfig = defineRootConfig({
-        basePath: import.meta.dirname
+  const basicRootConfig = defineRootConfig({
+    basePath: import.meta.dirname
+  })
+
+  it('expect: `template` config throw error when not passing `runtime`', () => {
+    const notPassConfig = () => defineTemplateConfig({
+      rootBasePath: basicRootConfig.baseRootPath
+    } as TemplateConfig)
+
+    expect(notPassConfig).toThrow()
+  })
+
+  it('expect: `template` config throw error when not passing `rootBasePath`', () => {
+    const notPassConfig = () => defineTemplateConfig({
+      runtime: 'node'
+    } as TemplateConfig)
+
+    expect(notPassConfig).toThrow()
+  })
+
+  it('expect: `template` config fallback `structures`', () => {
+    const config = defineTemplateConfig({
+      rootBasePath: basicRootConfig.baseRootPath,
+      runtime: 'node'
     })
 
-    it('expect: `template` config throw error when not passing `runtime`', () => {
-        const notPassConfig = () => defineTemplateConfig({
-            rootBasePath: basicRootConfig.baseRootPath
-        } as TemplateConfig)
+    expect(config.toRaw.structures).toStrictEqual([])
+  })
 
-        expect(notPassConfig).toThrow()
+  it('expect: `template` config fallback `tags`', () => {
+    const config = defineTemplateConfig({
+      rootBasePath: basicRootConfig.baseRootPath,
+      runtime: 'node'
     })
 
-    it('expect: `template` config throw error when not passing `rootBasePath`', () => {
-        const notPassConfig = () => defineTemplateConfig({
-            runtime: 'node'
-        } as TemplateConfig)
+    expect(config.toRaw.tags).toStrictEqual([])
+  })
 
-        expect(notPassConfig).toThrow()
+  it('expect: `template` config fallback `types`', () => {
+    const config = defineTemplateConfig({
+      rootBasePath: basicRootConfig.baseRootPath,
+      runtime: 'node'
     })
 
-    it('expect: `template` config fallback `structures`', () => {
-        const config = defineTemplateConfig({
-            rootBasePath: basicRootConfig.baseRootPath,
-            runtime: 'node'
-        })
-
-        expect(config.toRaw.structures).toStrictEqual([])
-    })
-
-    it('expect: `template` config fallback `tags`', () => {
-        const config = defineTemplateConfig({
-            rootBasePath: basicRootConfig.baseRootPath,
-            runtime: 'node'
-        })
-
-        expect(config.toRaw.tags).toStrictEqual([])
-    })
-
-    it('expect: `template` config fallback `types`', () => {
-        const config = defineTemplateConfig({
-            rootBasePath: basicRootConfig.baseRootPath,
-            runtime: 'node'
-        })
-
-        expect(config.toRaw.types).toStrictEqual([])
-    })
+    expect(config.toRaw.types).toStrictEqual([])
+  })
 });
